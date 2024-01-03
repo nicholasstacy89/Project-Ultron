9f3518c4fb1448c26aa7b26853e03d0843455e85
Chris Stevenson <chrislstevenson@gmail.com>
2024-01-03 07:41:45 -0500

    Reorganized and reformatted.
    Only substantive (coding) changes:
    - uncommented the line declaring a global variable for the Giffy API
    - wrapped the only executable code in a "document ready" block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,15 @@
-// var timeStamp = '1681802982683';
-// var pubKey = '228fa4b706c011a83b9bd0b501e5320d';
-// var hash = '830607a7cd9950993dac64c6f74ab021';
-// var characterName = 'spider-man';
-// //how to create a hash
-
-// function getMarvelData() {  
-//     var marvelURL = 'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith='+ characterName+ '&ts=' + timeStamp + '&apikey=' + pubKey + '&hash=' + hash;
-//     console.log(marvelURL); 
-//     fetch(marvelURL)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//             marvelApi(data);
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-        
-// };  
-// function marvelApi(data) {
-//     console.log(data);
-//         for (var i = 0; i < data.data.results.length; i++) {
-//         console.log(data.data.results[i].name);
-//         var marvelCard = $('<div>').addClass('singlecard tile is-child').attr('style', 'background-color: #34A8DA', 'margin: 10px','width: 18rem;');
-//         var list = document.getElementById('list');
-//         var charName = data.data.results[i].name;
-//         list.append(marvelCard);
-//         marvelCard.append($('<h3 class="card-title">').text(charName));
-      
-
-//     }};
-
-// getMarvelData();
-// Marvel API data fectch obsolite due to API not having the data we need.
-
+/*
+ * Global variables
+ */
 
 var btn = document.getElementById('search');
+var supername = document.getElementById('supername').value;
+var btn = document.getElementById('search');
+var giphyAPIkey = 'V8zVuDYLK9YXnITSBHQrcoevS2oMCdUY';
 
+/*
+ * Function declarations
+ */
 function getSuperData(superID) {
     fetch('https://www.superheroapi.com/api.php/10163066703485828/'+superID)
         .then(function (response) {
@@ -51,14 +22,12 @@ function getSuperData(superID) {
         .catch(function (error) {
             console.log(error);
         });
- }
-btn.addEventListener('click', getSuperData);
-var supername = document.getElementById('supername').value;
-var btn = document.getElementById('search');
+}
+
 function getID() {
-   
     console.log(supername);
-    fetch('https://www.superheroapi.com/api.php/10163066703485828/search/'+supername)        .then(function (response) {
+    fetch('https://www.superheroapi.com/api.php/10163066703485828/search/'+supername)
+        .then(function (response) {
             return response.json();
         })
         .then(function (data) {
@@ -68,22 +37,17 @@ function getID() {
         .catch(function (error) {
             console.log(error);
         });
- }
-btn.addEventListener('click', getID);
+}
 
 function usesuperID(data) {
     console.log(data);
     var superID = data.results[0].id;
     console.log(superID);
     getSuperData(superID);
-    
+
 }
 
-function createSuper(data) { 
-
-   
-        
-    
+function createSuper(data) {
     var superName = data.name;
     var eyes = data.appearance["eye-color"];
     var height = data.appearance.height[0];
@@ -102,15 +66,24 @@ function createSuper(data) {
     console.log('intelligence: '+superIntelligence);
     console.log('strenght: '+ superStrength);
     console.log('speed: '+ superSpeed);
-    
-
-    
-
 }
 
-createSuper();
+/*
+ * Event listeners
+ */
+
+btn.addEventListener('click', getSuperData);
+btn.addEventListener('click', getID);
+
+// Document ready
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    createSuper();
+
+}, false);  // end DOM ready
+
 // will get super data function to work before moving
-// var giphyAPIkey = 'V8zVuDYLK9YXnITSBHQrcoevS2oMCdUY';
 
 // function getGiphyData() {
 //     var giphyURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyAPIkey + '&q=' + supername;
@@ -126,5 +99,5 @@ createSuper();
 //         .catch(function (error) {
 //             console.log(error);
 //         });
-        
+
 // }
