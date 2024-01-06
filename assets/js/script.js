61183288b4bc6b41cd0125a7b894c96982b10bf4
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2024-01-06 10:56:11 -0500

    autocompete functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,88 @@
 var btn = document.getElementById('search');
+var showContainer = document.getElementById('list-container');
+var listContainer = document.getElementById('name-list');
+var input = document.getElementById('supername');
+var superList = []; 
+var superCard = document.getElementById('superCard');
 
 
-
-function getSuperData(superID) {
-    fetch('https://www.superheroapi.com/api.php/10163066703485828/'+superID)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            //console.log(data);
-            createSuper(data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+function displayName(name) {
+    input.value = name;
+    removeElements();
 }
+
+ removeElements = () => {
+   listContainer.innerHTML = '';
+ }
+
+input.addEventListener('keyup', async () => {
+    var url = 'https://www.superheroapi.com/api.php/10163066703485828/search/' +input.value;
+    var response = await fetch(url);
+    var JsonData = await response.json();
+    if (input.value.length <3) {
+        removeElements();
+        return;
+    }   
+    
+    if (input.value.length >= 3) {
+        console.log(JsonData);
+        console.log(url);
+        
+
+        JsonData.results.forEach(result => {
+        var checkArray = superList.includes(result.id);
+        if (checkArray === true) {
+            return;
+        }
+        else (checkArray === false); {
+        superList.push(result.id);
+        var superID = result.id;
+        var name = result.name;
+        var input = document.getElementById('supername');
+        var list = document.createElement('li');
+        list.style.color = 'black';
+        list.style.cursor = 'pointer';
+        list.style.overflow = 'auto';
+        list.style.textAlign = 'center';
+        list.style.margin = '1px';
+        list.style.padding = '1px';
+        list.style.listStyle = 'none';
+        list.style.fontSize = '10px';
+        list.style.fontFamily = 'monospace';
+        list.style.fontWeight = 'bold';
+        list.style.height = '15px';
+        list.style.width = 'auto';  
+        list.style.display = 'block'; 
+        list.style.backgroundColor = 'white';
+        list.style.borderRadius = '5px';    
+        list.id = result.id;
+        listContainer.style.display = 'block';
+        list.innerHTML = name;
+        
+        
+        listContainer.appendChild(list);
+        }
+
+
+
+        
+        list.addEventListener('click', () => getSuperData(superID));
+        }
+       )}});
+
+
+    
+      
+ 
+
+    
+
+
+
+
+
+
+
 
 
 function getID() {
@@ -33,7 +101,21 @@ function getID() {
     
 }
 
-
+function getSuperData(superID) {
+    fetch('https://www.superheroapi.com/api.php/10163066703485828/'+superID)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //console.log(data);
+            createSuper(data);
+            listContainer.style.display = 'none';
+            $(input).val('');
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 function usesuperID(data) {
     //console.log(data);
     var superID = data.results[0].id;
@@ -41,11 +123,14 @@ function usesuperID(data) {
     getSuperData(superID);
     
 }
+function clearSuperCard() {
+    $('#superCard').empty();
+}   
 
 function createSuper(data) { 
 
-   
-        
+    
+     clearSuperCard();   
     
     var superName = data.name;
     var eyes = data.appearance["eye-color"];
@@ -56,7 +141,7 @@ function createSuper(data) {
     var superStrength = data.powerstats.strength;
     var superSpeed = data.powerstats.speed;
     var cardName = $('<h1>').html(superName);
-
+    superCard.style.display = 'block';
     console.log('name: '+ superName);
     console.log('height: '+ height);
     console.log('hair: '+ hair);
